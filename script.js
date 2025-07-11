6080d93f153916bd0f4606a7676bfdb4c1cf58e8
wheresmyitinerary <amberjch@gmail.com>
2025-07-11 07:06:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Hamburger toggle
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('open');
+});
 
